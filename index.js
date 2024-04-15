eea3b3ece0d3da5f25d5e736c6b4f5494f4f54fc
Sarita Maurya <sm7767119@gmail.com>
2024-04-15 09:52:54 +0530

    nodemon

--- index.js
+++ index.js
@@ -18,4 +18,21 @@ app.get('/', (req, res) => {
   
   app.listen(port,()=>{
     console.log(`app is listening on port ${port}`);
+    console.log("sarita maurya");
+})
+app.get("/",(req,res)=>{
+  res.send("hello, i am root")
+})
+app.get("/:username/:id",(req,res)=>{
+    let {username, id} =req.params;
+    res.send(`Welcome to  the page of ${username} and id of ${id}`);
+})
+
+
+app.get("/search",(req,res)=>{
+  let { q } =( req.query);
+  if(!q){
+    res.send(`<h1>Nothing search</h1>`)
+  }
+  res.send(`<h1>search results for query: ${q}</h1>`)
 })
